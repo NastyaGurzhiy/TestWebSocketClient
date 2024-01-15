0ea2d153e5e76bf3d7a48ece333fea7ebe10cc6e
Anastasia Gurghii <a.gurzhyi@gmail.com>
2024-01-15 13:07:42 +0200

    Add project files.

---
+++ TestWebSocketClient/CurrencyPair.cs
@@ -0,0 +1,17 @@
+﻿using ProtoBuf;
+
+namespace TestWebSocketClient
+{
+    [ProtoContract]
+    public class CurrencyPair
+    {
+        [ProtoMember(1)]
+        public string Currency { get; set; }
+
+        [ProtoMember(2)]
+        public decimal Price { get; set; }
+
+        [ProtoMember(3)]
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ TestWebSocketClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using ProtoBuf;
+using System.Net.WebSockets;
+using TestWebSocketClient;
+
+var ws = new ClientWebSocket();
+await ws.ConnectAsync(new Uri("ws://localhost:6969/ws"), CancellationToken.None);
+Console.WriteLine("Connected!");
+
+var received = Task.Run(async() =>
+{
+    while (true)
+    {
+        var buffer = new ArraySegment<byte>(new byte[1024]);
+        var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+        if (result.MessageType == WebSocketMessageType.Close)
+            break;
+        var inputStream = new System.IO.MemoryStream(buffer.Array, 0, result.Count);
+        var currencyPair = Serializer.Deserialize<CurrencyPair>(inputStream);
+        Console.WriteLine(currencyPair);
+    }
+});
+await received;
